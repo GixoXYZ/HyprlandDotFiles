e0c5c8a0d7aa043d53ac5bf7cfef234db5f1760c
Gixo <notgixo@proton.me>
2023-11-01 21:39:14 +0330

    feat: add install script

---
+++ install.sh
@@ -0,0 +1,20 @@
+echo "Installing dotfiles..."
+echo "Copying files to /home directory..."
+
+cp .fonts.conf $HOME
+cp -r .local $HOME
+cp -r .config $HOME
+cp -r Wallpapers $HOME
+
+echo "Successfully copied files!"
+
+echo "You can now reboot your system to see changes"
+echo "Would you like to reboot now? (y/n)"
+
+read reboot
+
+if [ $reboot = "y" ]; then
+    sudo reboot
+else
+    echo "Alright, you can reboot later by typing 'sudo reboot'"
+fi
