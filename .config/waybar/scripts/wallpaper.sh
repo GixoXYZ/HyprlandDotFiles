5651c1256e92feaf65401567a4f823e392666d88
Gixo <24957924+GixoXYZ@users.noreply.github.com>
2024-02-19 20:50:18 +0330

    feat: update waybar confg

--- .config/waybar/scripts/wallpaper.sh
+++ .config/waybar/scripts/wallpaper.sh
@@ -2,23 +2,39 @@
 
 export PATH="${PATH}:${HOME}/.local/bin/"
 
-DIR=$HOME/Pictures/Wallpapers
-ACTIVE_WALLPAPER_PATH="$HOME/.cache/active-wallpaper-path"
-PICS=($(ls ${DIR}))
+DIR="$HOME/Pictures/Wallpapers"
+PICS=($(ls "$DIR"))
 
-RANDOMPICS=${PICS[$RANDOM % ${#PICS[@]}]}
+RANDOMPICS="${PICS[$RANDOM % ${#PICS[@]}]}"
 
-if [[ $(pidof swaybg) ]]; then
-  pkill swaybg
+if pidof swww >/dev/null; then
+  pkill swww
 fi
 
-sleep 1
+wait
 
-swaybg -i ${DIR}/${RANDOMPICS}
-wal -i ${DIR}/${RANDOMPICS} --cols16
+# Check if the lock file exists (indicating wal -i is running)
+if [ -f "$HOME/.wal_lock" ]; then
+  # Wait for wal -i to finish (indicated by absence of the lock file)
+  while [ -f "$HOME/.wal_lock" ]; do
+    sleep 1
+  done
+fi
+
+# swww init
+swww img "${DIR}/${RANDOMPICS}" --transition-type wipe --transition-fps 60 --transition-duration 0.3 --transition-angle 30 --transition-step 90 &
+
+wait
+
+# Create the lock file to indicate that wal -i is running
+touch "$HOME/.wal_lock"
+
+# wal command runs after swww is finished
+wal -i "${DIR}/${RANDOMPICS}" --cols16
+
+wait
+
+# Remove the lock file to indicate that wal -i has finished
+rm -f "$HOME/.wal_lock"
+
 pywalfox update
-
-echo "${DIR}/${RANDOMPICS}" >"$ACTIVE_WALLPAPER_PATH"
-. $HOME/.config/mako/update-colors.sh
-. $HOME/.config/spicetify/Themes/Pywal/update-colors.sh
-. $HOME/.config/cava/scripts/update-colors.sh
